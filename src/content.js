6ac4bf9cf1aaf7b50b3ec7be9e6408d91976a426
Elvis Oliveira <elvis.olv@gmail.com>
2023-12-22 20:02:55 +0100

    Enhanced parsing

--- src/content.js
+++ src/content.js
@@ -19,7 +19,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if(request.action === 'getSource') {
         const meetings = [];
 
-        document.querySelectorAll('div.pub-mwb:not(:has(> div#f1))').forEach((meeting, i) => {
+        document.querySelectorAll('div.pub-mwb:not(:has(> div#f1))').forEach((meeting) => {
             const data = {
                 date: getText(meeting, '[data-pid="1"]'),
                 theme: getText(meeting, '[data-pid="2"]'),
@@ -32,6 +32,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
             let section = null;
             Array.from(meeting.querySelector('div.bodyTxt').querySelectorAll('div, h3')).forEach((item) => {
+
                 item.classList.forEach((name) => {
                     if(sections.map((e) => e.icon).includes(name)) {
                         section = sections[sections.findIndex(e => e.icon === name)].acronym;
@@ -40,45 +41,55 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                 });
 
                 if(section && item.tagName === 'H3' && (/^\d+\./).test(item.textContent)) {
-                    const info = item.nextElementSibling.textContent.match(/\(([^()]*)\)/g); // Next element contains descr
-                    const title = item.textContent.match(/^(\d+)\.(.*)/); // Set part number apart
-                    data[section].push({
-                        time: getDigit(info.at(0)),
-                        title: title.at(2).trim(),
-                        number: getDigit(title.at(1))
-                    });
+                    /* ^\(                < matches ( on the beginning of the string
+                     *     ([^()]*)       < capturing group 1, everything but [ and ]
+                     * \)                 < matches )
+                     * \s*                < matches 0 or more whitespaces
+                     * ([^]*)             < capturing group 2, rest of the string
+                     */
+                    const info = item.nextElementSibling.textContent.trim().match(/^\(([^()]*)\)\s*([^]*)/); // next element contains descr
+                    const title = item.textContent.match(/^(\d+)\.(.*)/); // set part number apart
+                    const number = getDigit(title[1]);
+                    const entry = {
+                        time: getDigit(info[1]),
+                        title: title[2].trim(),
+                        number
+                    };
+                    if(info[2]) {
+                        const description = info[2].trim();
+                        /* ([^]*)\(            < capturing group 1, everything untill (
+                         * ((?:lmd|th).*)      < capturing group 2, everything starting with lmd or th
+                         * \)$                 < matches ) right before the end of line
+                         */
+                        const hasLesson = description.match(/([^]*)\(((?:lmd|th).*)\)$/);
+                        if(hasLesson) {
+                            entry.lesson = hasLesson[2].trim();
+                            if(number === 3) { // number 3 is always bible reading, add assignment
+                                entry.assignment = hasLesson[1].trim();
+                            } else {
+                                const isTalk = hasLesson[1].trim().match(/\—(?:[^]*)\:(.*)$/);
+                                if(isTalk) {
+                                    entry.theme = isTalk[1].trim();
+                                }
+                            }
+                        }
+                    }
+                    data[section].push(entry);
                 }
             });
 
             meetings.push({
                 date: data.date,
                 theme: data.theme,
-                chairman: '',
                 opening_song: data.songs[0],
-                opening_talk: {
-                    speaker: '',
-                    theme: data.tfgw[0].title
-                },
-                spiritual_gems: '',
-                bible_reading: {
-                    reader: '',
-                    assignment: '' // @TODO: Parse
-                },
-                apply_yourself_to_the_field_ministry: data.ayttfm.map(elem => ({
-                    ...elem,
-                    // @TODO: Figure out if it is a talk
-                    speaker: '',
-                    assigned: '',
-                    assistant: ''
-                })),
+                opening_talk: data.tfgw[0],
+                spiritual_gems: data.tfgw[1],
+                bible_reading: data.tfgw[2],
+                apply_yourself_to_the_field_ministry: data.ayttfm,
                 middle_song: data.songs[1],
-                living_as_christians: data.lac.map(elem => ({ ...elem, speaker: '' })).slice(0, -1),
-                congregation_bible_study: {
-                    reader: '',
-                    conductor: ''
-                },
-                closing_song: data.songs[2],
-                closing_prayer: ''
+                living_as_christians: data.lac.slice(0, -1),
+                congregation_bible_study: data.lac[data.lac.length - 1],
+                closing_song: data.songs[2]
             });
         });
 
--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,8 @@
 import { prettyPrintJson } from 'pretty-print-json';
 
-window.addEventListener("load", () => {
-    const elem = document.getElementById('code');
+const code = document.getElementById('code');
+
+window.addEventListener('load', () => {
     chrome.tabs.query({
         url: [
             'https://wol.jw.org/*/wol/meetings/*',
@@ -10,17 +11,40 @@ window.addEventListener("load", () => {
         active: true
     }, (tabs) => {
         if(tabs.length > 0) {
-            tabs.forEach((tab) => {
-                chrome.tabs.sendMessage(tab.id, { action: 'getSource' }, (response) => {
-                    elem.innerHTML = prettyPrintJson.toHtml(response, {
-                        indent: 2,
-                        quoteKeys: true,
-                        trailingComma: false
-                    });
-                });
+            tabs.forEach(tab => {
+                chrome.tabs.sendMessage(tab.id, { action: 'getSource' }, meeting => chrome.storage.local.set({ meeting }, () => render(false)));
             });
         } else {
-            elem.innerHTML = '<span id="error">You must be in the meetings page</span>';
+            code.innerHTML = '<span id="error">You must be in the meetings page</span>';
         }
     });
 }, false);
+
+document.getElementById('empty').addEventListener('change', (e) => render(e.currentTarget.checked));
+
+const render = (assignments) => {
+    chrome.storage.local.get('meeting', (data) => {
+        const { meeting } = data;
+        if(assignments) {
+            for (var i = 0; i < meeting.length; i++) {
+                meeting[i] = Object.assign({ chairman: '' }, meeting[i], { closing_prayer: '' });
+                meeting[i]['opening_talk']['speaker'] = '';
+                meeting[i]['spiritual_gems']['conductor'] = '';
+                meeting[i]['bible_reading']['reader'] = '';
+                meeting[i]['congregation_bible_study']['conductor'] = '';
+                meeting[i]['congregation_bible_study']['reader'] = '';
+            }
+        }
+        // const empty = {
+        //     // apply_yourself_to_the_field_ministry: data.ayttfm.map(elem => ({ speaker: '', assigned: '', assistant: '' })),
+        //     // living_as_christians: data.lac.map(elem => ({ ...elem, /* speaker: '' */ })).slice(0, -1),
+        // };
+        code.innerHTML = prettyPrintJson.toHtml(meeting, {
+            indent: 2,
+            quoteKeys: true,
+            trailingComma: false
+        });
+    });
+};
+
+window.addEventListener('unload', () => chrome.storage.local.remove('meeting'));
